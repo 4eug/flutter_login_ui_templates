06365408fab3be30ed62226d96c9d482bc5d07ae
4eug <debraheug@gmail.com>
2021-04-20 08:54:32 +0100

    HomeScreen Added

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      )),
+    );
+  }
+}

2c42fb9a914084102f1c6cf77324569eaed33c09
4eug <debraheug@gmail.com>
2021-04-20 09:21:12 +0100

    ...

---
+++ lib/utils/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Fonts {
+  static const primaryFont = "Poppins";
+}
+
+//Icons
+class AvailableIcons {
+  static const menuIcon = {
+    'assetImage': AssetImage('assets/images/menu.png'),
+    'assetPath': 'assets/images/menu.png',
+  };
+
+  static const backIcon = {
+    'assetImage': AssetImage('assets/images/backArrow.png'),
+    'assetPath': 'assets/images/backArrow.png',
+  };
+}
+
+class AvailableImages {
+  static const appLogo = {
+    'assetImage': AssetImage('assets/images/appLogo.png'),
+    'assetPath': 'assets/images/appLogo.png',
+  };
+
+  static const background = {
+    'assetImage': AssetImage(''),
+    'assetPath': 'assets/images/background.jpg',
+  };
+
+  static const reddit_login = {
+    'assetImage': AssetImage(''),
+    'assetPath': 'assets/images/inspire_1.png',
+  };
+
+  static const pirple_login = {
+    'assetImage': AssetImage('assets/images/phantom_2_1.png'),
+    'assetPath': 'assets/images/phantom_2_1.png',
+  };
+}

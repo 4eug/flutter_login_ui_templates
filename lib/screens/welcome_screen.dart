2a0ee1206de572095f198beba182b381336f1e08
4eug <debraheug@gmail.com>
2021-04-20 09:02:09 +0100

    Welcome Screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  WelcomeScreen({Key key}) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

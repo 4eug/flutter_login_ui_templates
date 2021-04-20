7a11ce74aff715e0cafd45899271c7609699e4de
4eug <debraheug@gmail.com>
2021-04-20 13:35:15 +0100

    System Orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_login_template_ui/routes/routes.dart';
 
 void main() {
@@ -9,9 +10,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       title: 'Login Screens',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       initialRoute: welcomeScrenRoute,
     );
   }

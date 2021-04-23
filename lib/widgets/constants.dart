c1d7f3709d3b50145c8f30d2898df2913bdc306a
4eug <debraheug@gmail.com>
2021-04-23 10:00:58 +0100

    Ui Screens

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -2,19 +2,8 @@ import 'package:flutter/material.dart';
 
 class UIHelper {
   //Images
-  static final String muzPhoto = "assets/images/muz_login.png";
-  static final String grapePhoto = "assets/images/grape_login.png";
-  static final String strawberryPhoto = "assets/images/strawberry_login.png";
-  static final String blueberryPhoto = "assets/images/blueberry.png";
-  static final String pomegranatePhoto = "assets/images/pomegranate_login.png";
-  static final String apricotPhoto = "assets/images/apricot_login.png";
-  static final String figPhoto = "assets/images/fig_login.png";
-  static final String cherryPhoto = "assets/images/cherry_login.png";
-  static final String applePhoto = "assets/images/apple_login.png";
-  static final String watermelonPhoto = "assets/images/watermelon_login.png";
-  static final String pineapplePhoto = "assets/images/pineapple_login.png";
-  static final String pearPhoto = "assets/images/pear_login.png";
-  static final String noPhoto = "assets/images/no_photo.png";
+  static final String grapple = "assets/ui/Grapple_login_ui.jpg";
+
   static final String background = "assets/images/background.jpg";
 
   // Global Colors

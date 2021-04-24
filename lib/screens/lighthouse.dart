1a437aab0257c49cf70f327a8a1ec670de04938a
4eug <debraheug@gmail.com>
2021-04-24 12:57:43 +0100

    On Lighthous Login Screen

--- lib/screens/lighthouse.dart
+++ lib/screens/lighthouse.dart
@@ -18,7 +18,7 @@ class _LightHouseLoginState extends State<LightHouseLogin> {
         child: Stack(
           children: [
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 120),
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 80),
               color: Color(0xFFfafafa),
               width: double.infinity,
               child: Column(
@@ -27,24 +27,59 @@ class _LightHouseLoginState extends State<LightHouseLogin> {
                   Center(
                     child: Image.asset(
                       UIGuide.lighthouse_image,
+                      height: SizeConfig.blockSizeVertical * 30,
+                      width: SizeConfig.blockSizeHorizontal * 90,
                     ),
                   ),
                   SizedBox(height: SizeConfig.blockSizeVertical * 3),
                   Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
                     mainAxisSize: MainAxisSize.max,
                     children: [
                       Text(
                         "Flutter Login Template",
                         style: GoogleFonts.roboto(
-                          fontSize: 25,
+                          fontSize: 30,
                           color: Colors.black,
                           fontWeight: FontWeight.w500,
                         ),
-                      )
+                      ),
                     ],
-                  )
+                  ),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "Login Ui easy to use",
+                        style: GoogleFonts.roboto(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 0.5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "for your next mobile project.",
+                        style: GoogleFonts.roboto(
+                          fontSize: 20,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 5),
+                  _googlelogin(context),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 3),
+                  _fblogin(context)
                 ],
               ),
             )
@@ -53,4 +88,50 @@ class _LightHouseLoginState extends State<LightHouseLogin> {
       ),
     );
   }
+}
+
+Widget _googlelogin(context) {
+  return Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      color: Colors.red,
+    ),
+    // ignore: deprecated_member_use
+    child: RaisedButton(
+      onPressed: () => {},
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Text(
+        "Continue With Google +",
+        style: GoogleFonts.roboto(
+            fontSize: 20,
+            color: Colors.white,
+            letterSpacing: 0.168,
+            fontWeight: FontWeight.w500),
+      ),
+      color: Colors.red,
+    ),
+  );
+}
+
+Widget _fblogin(context) {
+  return Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      color: Colors.blue,
+    ),
+    // ignore: deprecated_member_use
+    child: RaisedButton(
+      onPressed: () => {},
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Text(
+        "Continue With Facebook",
+        style: GoogleFonts.roboto(
+            fontSize: 20,
+            color: Colors.white,
+            letterSpacing: 0.168,
+            fontWeight: FontWeight.w500),
+      ),
+      color: Colors.blue,
+    ),
+  );
 }

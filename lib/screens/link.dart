3feabde04a834ca3d76d8483788576de9ca88fdf
4eug <debraheug@gmail.com>
2021-04-25 08:33:09 +0100

    On Link Login

--- lib/screens/link.dart
+++ lib/screens/link.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login_template_ui/widgets/constants.dart';
 
 class LinkLogin extends StatefulWidget {
   LinkLogin({Key key}) : super(key: key);
@@ -10,6 +11,28 @@ class LinkLogin extends StatefulWidget {
 class _LinkLoginState extends State<LinkLogin> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 60, horizontal: 30),
+              color: Colors.white,
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.only()),
+                  Center(
+                    child: Image.asset(
+                      UIGuide.link_head,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

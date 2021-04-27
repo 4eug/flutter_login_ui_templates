ca243bd1c8d579c1f8da3ad50a679677b31f6f9b
4eug <debraheug@gmail.com>
2021-04-27 23:33:18 +0100

    pirple login clone

--- lib/screens/pirple.dart
+++ lib/screens/pirple.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login_template_ui/utils/config.dart';
 import 'package:flutter_login_template_ui/widgets/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PirpleLogin extends StatefulWidget {
   PirpleLogin({Key key}) : super(key: key);
@@ -18,20 +19,42 @@ class _PirpleLoginState extends State<PirpleLogin> {
         child: Stack(
           children: [
             Container(
-              
+              color: Colors.black,
               width: double.infinity,
               child: Column(
                 children: [
                   Padding(padding: EdgeInsets.only()),
-                  Image.asset(
-                    UIGuide.pirple_img,
-                    height: SizeConfig.blockSizeVertical * 100,
-                    width: SizeConfig.blockSizeHorizontal * 100,
-                    fit: BoxFit.fitHeight,
-                  )
+                  ColorFiltered(
+                    child: Image.asset(
+                      UIGuide.pirple_img,
+                      height: SizeConfig.blockSizeVertical * 100,
+                      width: SizeConfig.blockSizeHorizontal * 100,
+                      fit: BoxFit.fitHeight,
+                    ),
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), BlendMode.dstATop),
+                  ),
                 ],
               ),
-            )
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 200),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    "Pirple Login Ui",
+                    style: GoogleFonts.poppins(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: SizeConfig.blockSizeVertical * 5),
           ],
         ),
       ),

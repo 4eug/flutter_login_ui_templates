18e8e4b1e1d6a29b67c26ef5c17a24854439616b
4eug <debraheug@gmail.com>
2021-04-29 15:05:11 +0100

    Login login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login_template_ui/utils/config.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   Login({Key key}) : super(key: key);
@@ -10,6 +12,70 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+              color: Color(0xFFfafafa),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.only()),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "Sign In",
+                        style: GoogleFonts.poppins(
+                          fontSize: 30,
+                          color: Color(0xff07446C),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "Lorem ipsum dolor sit amet, consetetur",
+                        style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          color: Color(0xff07446C),
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 0.5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "sadipscing elitr, sed diam nonumy eirmod  ",
+                        style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          color: Color(0xff07446C),
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: SizeConfig.blockSizeVertical * 5),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

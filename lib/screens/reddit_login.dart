251c5add943b7a7b897355df697bc3980c6be6cf
4eug <debraheug@gmail.com>
2021-04-30 09:04:50 +0100

    Video Player for Reddit login

--- lib/screens/reddit_login.dart
+++ lib/screens/reddit_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class RedditLogin extends StatefulWidget {
   @override
@@ -6,8 +7,37 @@ class RedditLogin extends StatefulWidget {
 }
 
 class _RedditLoginState extends State<RedditLogin> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("")
+      ..initialize().then((_) {
+        _controller.play();
+        _controller.setLooping(true);
+
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: _controller.value.size?.width ?? 0,
+                height: _controller.value.size?.height ?? 0,
+                child: VideoPlayer(_controller),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

8fe2e1ab3492d8eca9477c080ef1ac86843abafa
4eug <debraheug@gmail.com>
2021-05-05 22:00:40 +0100

    On Pattern Login Almost

--- lib/screens/patterns.dart
+++ lib/screens/patterns.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login_template_ui/utils/config.dart';
 import 'package:flutter_login_template_ui/widgets/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PatternsLogin extends StatefulWidget {
   @override
@@ -41,26 +42,27 @@ class _PatternsLoginState extends State<PatternsLogin> {
           height: double.infinity,
           decoration: BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(60), topRight: Radius.circular(60))),
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(60))),
           child: Padding(
             padding: const EdgeInsets.all(40.0),
             child: SingleChildScrollView(
+              physics: NeverScrollableScrollPhysics(),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  new Text(
-                    "Hello",
-                    style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1),
+                  Center(
+                    child: Text("Login",
+                        style: GoogleFonts.poppins(
+                          fontSize: 30,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                        )),
                   ),
-                  new Text("Create an account to continue"),
                   _textField(context),
                   _textField(context),
-                  _loginButton,
+                  SizedBox(height: 20),
+                  _loginbtn(context),
                 ],
               ),
             ),
@@ -86,40 +88,26 @@ class _PatternsLoginState extends State<PatternsLogin> {
         ),
       );
 
-  Widget get _loginButton => Center(
-        child: Padding(
-            padding: EdgeInsets.only(top: 70),
-            child: SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.red,
-                      blurRadius:
-                          10.0, // has the effect of softening the shadow
-                      spreadRadius:
-                          1.0, // has the effect of extending the shadow
-                      offset: Offset(
-                        0.0, // horizontal, move right 10
-                        5.0, // vertical, move down 10
-                      ),
-                    ),
-                  ],
-                ),
-                // ignore: deprecated_member_use
-                child: RaisedButton(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(50.0)),
-                  onPressed: () {},
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontSize: 20, color: Colors.green),
-                  ),
-                ),
-              ),
-            )),
-      );
+  Widget _loginbtn(context) {
+    // ignore: deprecated_member_use
+    return Center(
+      // ignore: deprecated_member_use
+      child: FlatButton(
+        onPressed: () => {},
+        padding: EdgeInsets.symmetric(vertical: 18, horizontal: 120),
+        shape: new RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(40.0),
+        ),
+        child: Text(
+          "LOG IN",
+          style: GoogleFonts.montserrat(
+              fontSize: 23,
+              color: Colors.white,
+              letterSpacing: 0.168,
+              fontWeight: FontWeight.w500),
+        ),
+        color: Colors.yellowAccent,
+      ),
+    );
+  }
 }
